912eedfc11eec92a430176ae784ccc4b2e50b125
Jaime Soriano Pastor <jsoriano@tuenti.com>
2012-05-31 23:29:16 +0200

    Timeout can be set to custom values

--- test_client.py
+++ test_client.py
@@ -1,7 +1,11 @@
+import time
+
 from housekeeper import HousekeeperClient
 
 c = HousekeeperClient()
 c.set('mercurial', 'fooobar')
 print c.get('mercurial')
-c.set('mercurial', 'stuff', 10)
+c.set('mercurial', 'stuff', 1)
+print c.get('mercurial')
+time.sleep(3)
 print c.get('mercurial')
